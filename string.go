c3f5b5c937d17c216ccc4bf44e7fec3c84f39e4b
nanandt <rizkyfatih007@gmail.com>
2021-12-30 22:05:47 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Fahmi")
+	fmt.Println(len("Fahmi"))
+
+	fmt.Println("Fahmi Kurniawan")
+	fmt.Println(len("Fahmi Kurniawan"))
+
+	fmt.Println("Fahmi bae"[4])
+}
