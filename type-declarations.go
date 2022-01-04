658912af18834b0f983bbbc3f1eee951e5d031de
nanandt <rizkyfatih007@gmail.com>
2022-01-04 21:42:44 +0700

    type-declaration

--- type-declarations.go
+++ type-declarations.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	type NoKTP string
+	type Merried bool
+
+	var NoKtpRizky NoKTP = "982329832983788"
+	fmt.Println(NoKtpRizky)
+
+	var merriedStatus Merried = false
+	fmt.Println(merriedStatus)
 }
