a82af3556e173981a17203a92554104fc6fab511
nanandt <rizkyfatih007@gmail.com>
2022-01-04 21:42:18 +0700

    variable

--- variable.go
+++ variable.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	var name string // cara deklarasi variable ke 1
+
+	name = "wahyu aji leksono"
+	fmt.Println(name)
+
+	name = "jaka harun"
+	fmt.Println(name)
+
+	var country = "indonesia" // cara deklarasi variable ke 2
+	fmt.Println(country)
+
+	var age = 70
+	fmt.Println(age)
+
+	firstName := "muhamad" // cara deklarasi variable ke 3, dan hanya untuk deklarasi awal saja
+	fmt.Println(firstName)
+
+	var ( // multiple variable
+		lastName = "fatikhurrizky"
+		middleName = "kechaw"
+	)
+
+	fmt.Println(lastName)
+	fmt.Println(middleName)
 }
