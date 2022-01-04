3b75c125dd169374ffc132b6c96c96e9b331f1be
nanandt <rizkyfatih007@gmail.com>
2022-01-04 21:41:57 +0700

    conversion

--- conversion.go
+++ conversion.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	var nilai32 int32 = 128
+	var nilai64 int64 = int64(nilai32)
+	var nilai8 int8 = int8(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+	fmt.Println(nilai8)
+
+	name := "rizky"
+	e := name[0]
+	eString := string(e)
+
+	fmt.Println(e)
+	fmt.Println(eString)
 }
