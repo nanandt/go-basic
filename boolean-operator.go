a2f93c9b4cecec58056803c4e97afd8757030788
nanandt <rizkyfatih007@gmail.com>
2022-01-13 00:16:19 +0700

    boolean operator

--- boolean-operator.go
+++ boolean-operator.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	ujian := 80
+	absen := 70
+
+	//lulusUjian := ujian >= 80 // true
+	//lulusAbsen := absen > 70 // false
+	//fmt.Println(lulusUjian)
+	//fmt.Println(lulusAbsen)
+	//
+	//var naikKelas bool = lulusUjian && lulusAbsen
+	//fmt.Println(naikKelas)
+
+	fmt.Println(ujian >= 80 && absen > 70)
+
+	
 }
