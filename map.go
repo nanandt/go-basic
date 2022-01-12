bed9e6d81d20fc9e923dc50534aa63f93176d7dd
nanandt <rizkyfatih007@gmail.com>
2022-01-12 23:51:19 +0700

    map

--- map.go
+++ map.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+
+	person := map[string]string{
+		"name":    "Rizky",
+		"address": "tegal",
+	}
+	person["title"] = "progammer"
+	person["title"] = "progammer anyar"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+	fmt.Println(person["title"])
+
+	book := make(map[string]string)
+	book["title"] = "Buku Cara Ngoding"
+	book["author"] = "Anak Singkong"
+	book["age"] = "22"
+
+	delete(book, "age")
+
+	fmt.Println(book["title"])
+	fmt.Println(book["author"])
+	fmt.Println(book["age"])
 }
