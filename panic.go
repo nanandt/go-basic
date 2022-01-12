c8ad94f2dd243229b0e209ea51076afa5a57b3ad
nanandt <rizkyfatih007@gmail.com>
2022-01-13 00:17:07 +0700

    panic

--- panic.go
+++ panic.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+func endApp() {
+	message := recover()
+	if message != nil {
+		fmt.Println("Terjadi Error dengan :", message)
+	}
+	fmt.Println("App Selesai")
+}
+
+
+// panic mengentikan progam tetapi defer akan tetap diexecute
+// recover tetap menjalankn app meski teerjadi panic dan menangkap pesan panic tsb
+// dan menyimpan nya di defer func
+
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("Aplikasi Error")
+	}
+	fmt.Println("Aplikasi berjalan")
+}
+
 func main() {
-	$END$
+	runApp(true)
+	fmt.Println("rizky")
 }
