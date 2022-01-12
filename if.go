7ee56c9c9bc9d6a9e8e7fb77f52f14e9b9052961
nanandt <rizkyfatih007@gmail.com>
2022-01-12 23:51:39 +0700

    if

--- if.go
+++ if.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	name := "hendr"
+
+	if name == "rizky"{
+		fmt.Println("Hello", name)
+	} else if name == "rizzky" {
+		fmt.Println("Hello", name)
+	} else {
+		fmt.Println("kamu Siapaaa?", name)
+	}
+
+	if length := len(name); length > 5 {
+		fmt.Println("nama terlalu panjang")
+	} else {
+		fmt.Println("nama sudah cukup")
+	}
+
+	
 }
