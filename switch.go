27b2ff708caebbb8047f94aed1b7302601e870e7
nanandt <rizkyfatih007@gmail.com>
2022-01-12 23:52:09 +0700

    switch

--- switch.go
+++ switch.go
@@ -1,5 +1,36 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	name := "rizky"
+
+	switch name {
+	case "rizky":
+		fmt.Println("Hello", name)
+	case "Gaga":
+		fmt.Println("Hello", name)
+	default:
+		fmt.Println("Kamu siapaaa?")
+	}
+	// short statement
+	//switch length := len(name); length > 5 {
+	//case true:
+	//	fmt.Println("Nama terlalu panjang")
+	//case false:
+	//	fmt.Println("Nama sudah benar")
+	//}
+
+	// switch tanpa kondisi
+	length := len(name)
+	switch {
+	case length > 5:
+		fmt.Println("kepanjangan boss")
+	case length <= 5:
+		fmt.Println("nama sudah cukup")
+	default:
+		fmt.Println("nama benar")
+	}
+
+
 }
