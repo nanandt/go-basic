e090cdd30a973d38a474c73d23bcd188239e6b11
nanandt <rizkyfatih007@gmail.com>
2022-01-16 22:19:29 +0700

    closure

--- closure.go
+++ closure.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	name := "rizky"
+	counter := 0
+	increment := func() {
+		name = "hasan"
+		fmt.Println("increment")
+		counter++
+	}
+
+	increment()
+	increment()
+	fmt.Println(counter)
+	fmt.Println(name)
+
+	
 }
