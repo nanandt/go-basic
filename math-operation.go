c13576e03a48dae4e65e8a150d1476ff75107528
nanandt <rizkyfatih007@gmail.com>
2022-01-16 22:22:09 +0700

    math-operation

--- math-operation.go
+++ math-operation.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	a := 5
+	b := 10
+	c := a *b
+	fmt.Println(c)
+
+	// augmented assignments
+	d := 20
+	d *= 50 // d = d * 50
+	fmt.Println(d)
+	
 }
