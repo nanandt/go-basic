5794c19087981bd9fc17f136440ef098c25f3509
nanandt <rizkyfatih007@gmail.com>
2022-01-16 22:20:10 +0700

    comparation-operator

--- comparation-operator.go
+++ comparation-operator.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	name1 := "wahyu"
+	name2 := "wahyu"
+	var result bool = name1 == name2
+	fmt.Println(result)
+
+	value1 := 500
+	value2 := 300
+	fmt.Println(value1 > value2)
+	fmt.Println(value1 < value2)
+	fmt.Println(value1 == value2)
+	fmt.Println(value1 != value2)
 }
