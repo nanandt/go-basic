7de6c094189b2fe6bcb0024838e8b7cca1e9bf61
nanandt <rizkyfatih007@gmail.com>
2022-01-20 22:37:59 +0700

    function-anonymous

--- function-anonymous.go
+++ function-anonymous.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, b Blacklist) {
+	if b(name) {
+		fmt.Println("You are Blocked ", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
 func main() {
-	$END$
+	blacklist := func(name string) bool {
+		return name == "admin"
+	}
+
+	registerUser("admin", blacklist)
+	registerUser("rizky", blacklist)
+
+	registerUser("root", func(name string) bool {
+		return name == "root"
+	})
 }
